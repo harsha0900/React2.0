fc0665a8adbaab6baf7d403035b0ce8111472183
harsha0900 <reddyharsha552@gmail.com>
2024-04-06 14:40:36 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "this is react separated JS"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
